6a6786f516966f4236702bb2e237839694513312
jeliang1111 <92902705+jeliang1111@users.noreply.github.com>
2023-02-04 15:28:25 -0500

    hi rakesh

--- background.js
+++ background.js
@@ -1,20 +1,39 @@
 
+async function getCommittee(){
 
-// activate extension when host is www.website.com
-const { Configuration, OpenAIApi } = require("openai");
+    const { Configuration, OpenAIApi } = require("openai");
 
-const configuration = new Configuration({
-apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+    const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
 
-const response = await openai.createCompletion("text-curie-001", {
-prompt: "Write a blog post on vadala onions",
-temperature: 0.7,
-max_tokens: 256,
-top_p: 1,
-frequency_penalty: 0,
-presence_penalty: 0,
-});
+    const response = await openai.createCompletion("text-curie-001", {
+    prompt: "Write a blog post on vadala onions",
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    });
 
-console.log(response.choices[0].text);
+    
+
+    console.log(response.choices[0].text);
+}
+
+function sendtoAI(domContent) {
+    console.log(domContent)
+    // let json_data = {
+    //     title: JSON.parse(domContent).title,
+    //     h1: JSON.parse(domContent).h1,
+    //     body: JSON.parse(domContent).body,
+    // };
+
+}
+
+startNavigation.onclick.addListener(function(element) {
+
+    chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, sendtoAI)
+
+})
--- popup.js
+++ popup.js
@@ -1,40 +1,13 @@
 'use strict'
 
 
-startNavigation.onclick = function(element) {
 
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, async function(tabs) {
-        getText(tabs[0].url)
-    });
 
-}
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 
-async function getText(url){
+    if(msg.text == "report back"){
 
-    chrome.tabs.update({url: url});
-    chrome.runtime.onMessage.addListener(function getDOMInfo(message) {
-        // remove onMessage event as it may get duplicated
-        chrome.runtime.onMessage.removeListener(getDOMInfo);
-
-        // save data from message to a JSON file and download
-        let json_data = {
-            title: JSON.parse(message).title,
-            h1: JSON.parse(message).h1,
-            url: url
-        };
-
-        let blob = new Blob([JSON.stringify(json_data)], {type: "application/json;charset=utf-8"});
-        let objectURL = URL.createObjectURL(blob);
-        chrome.downloads.download({ url: objectURL, filename: ('content/' + url_index + '/data.json'), conflictAction: 'overwrite' });
-    });
-
-    // execute content script
-    chrome.tabs.executeScript({ file: 'script.js' }, function() {
-        // resolve Promise after content script has executed
-        resolve();
-    });
-}
+        sendResponse(document[0].outerHTML);
+    }
+})
 
